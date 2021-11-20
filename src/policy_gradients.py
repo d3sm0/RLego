128bc125e313fac18353858b348c63ed8c701887
d3sm0 <simone.totaro@gmail.com>
2021-11-19 22:02:31 -0500

    Fix td objective

--- src/policy_gradients.py
+++ src/policy_gradients.py
@@ -1,11 +1,11 @@
 import torch.distributions as torch_dist
 
 
-def vanilla_policy_gradient(log_prob, a_t, q_t):
+def vanilla_policy_gradient(log_prob, q_t):
     return -log_prob * (q_t).detach()
 
 
 def softmax_policy_gradient(logits, a_t, q_t):
-    log_prob = torch_dist.Categorical(logits=logits).logp_rob(a_t.detach())
+    log_prob = torch_dist.Categorical(logits=logits).log_prob(a_t.detach())
     return vanilla_policy_gradient(log_prob, q_t)
 
