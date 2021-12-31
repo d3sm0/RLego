50966af05ac1cc7005eb525edd7b3d34eaed2bf2
manuel <manuel.delverme@gmail.com>
2021-12-31 12:05:03 -0500

    moved trajectory unpacking to library. there is no need to hide away data and then return it as it is with a getter Trajectory.data should be enough, get_traj now does the unpacking we often need to do anyway.

--- rlego/src/buffer.py
+++ rlego/src/buffer.py
@@ -19,28 +19,33 @@ class Transition:
 
 class Trajectory:
     def __init__(self):
-        self._data = []
+        self.data = []
 
     def __len__(self):
-        return len(self._data)
+        return len(self.data)
 
     def append(self, transition: Transition):
-        self._data.append(transition)
+        self.data.append(transition)
 
     def __iter__(self):
-        return iter(self._data)
-
-    def get_trajectory(self):
-        return self._data
+        return iter(self.data)
 
     def get_partial(self, start_idx, horizon):
         horizon = min(self.__len__() - start_idx, horizon)
-        return self._data[start_idx:start_idx + horizon]
+        return self.data[start_idx:start_idx + horizon]
 
-    def sample(self, batch_size:int=1, horizon:int=1):
-        start_idxs = torch.randint(self.__len__() - 1, (batch_size, ))
+    def sample(self, batch_size: int = 1, horizon: int = 1):
+        start_idxs = torch.randint(self.__len__() - 1, (batch_size,))
         # TODO we should be able to have something N X T but it seems hard for now we keep it like this
         return [self.get_partial(start_idx, horizon) for start_idx in start_idxs][0]
+
+    def get_trajectory(self) -> Tuple[torch.tensor, torch.tensor, torch.tensor, torch.tensor, torch.tensor, List[dict]]:
+        r_t, a_t, s_t, s_tp1, infos = list(zip(*self.data))
+        r_t = torch.tensor(r_t)
+        a_t = torch.stack(a_t, 0)
+        s_t = torch.stack(s_t, 0)
+        s_tp1 = torch.stack(s_tp1, 0)
+        return r_t, a_t, s_t, s_tp1, infos
 
 
 class Buffer:
@@ -65,7 +70,7 @@ class Buffer:
         self._next_idx = (self._next_idx + 1) % self._buffer_size
 
     def sample(self, batch_size: int):
-        idxes = torch.randint(len(self._data) - 1, (batch_size, ))
+        idxes = torch.randint(len(self._data) - 1, (batch_size,))
 
         batch = [self._data[idx] for idx in idxes]
         batch = list(map(lambda x: torch.stack(x), list(zip(*batch))))
