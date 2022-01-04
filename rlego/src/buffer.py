d0ded3932020b20897d30bd115be9404b4ade4d7
Manuel <manuel.delverme@gmail.com>
2022-01-03 23:44:14 -0500

    Trajectory interface (#1)
    Trajectory handles an extended `Transition` with an `info` attributes for specific use cases. Minor cleanup done on the way.

--- rlego/src/buffer.py
+++ rlego/src/buffer.py
@@ -1,4 +1,5 @@
 import dataclasses
+from typing import List, Union
 
 import torch
 
@@ -10,7 +11,7 @@ class Transition:
     reward: torch.tensor
     next_state: torch.tensor
     done: torch.tensor
-    info: dict
+    info: Union[dict, List[dict]]
 
     def __iter__(self):
         for attr in ["state", "action", "reward", "next_state", "done", "info"]:
@@ -19,28 +20,34 @@ class Transition:
 
 class Trajectory:
     def __init__(self):
-        self._data = []
+        self.data: List[Transition] = []
 
     def __len__(self):
-        return len(self._data)
+        return len(self.data)
 
     def append(self, transition: Transition):
-        self._data.append(transition)
+        self.data.append(transition)
 
     def __iter__(self):
-        return iter(self._data)
+        return iter(self.data)
 
-    def get_trajectory(self):
-        return self._data
-
-    def get_partial(self, start_idx, horizon):
+    def get_partial(self, start_idx, horizon) -> List[Transition]:
         horizon = min(self.__len__() - start_idx, horizon)
-        return self._data[start_idx:start_idx + horizon]
+        return self.data[start_idx:start_idx + horizon]
 
     def sample(self, batch_size: int = 1, horizon: int = 1):
         start_idxs = torch.randint(self.__len__(), (batch_size, ))
         # TODO we should be able to have something N X T but it seems hard for now we keep it like this
         return [self.get_partial(start_idx, horizon) for start_idx in start_idxs][0]
+
+    def get_trajectory(self) -> Transition:
+        states, actions, rewards, next_states, dones, infos = list(zip(*self.data))
+        states = torch.stack(states, 0)
+        actions = torch.stack(actions, 0)
+        rewards = torch.tensor(rewards)
+        next_states = torch.stack(next_states, 0)
+        dones = torch.tensor(dones)
+        return Transition(states, actions, rewards, next_states, dones, infos)
 
 
 class Buffer:
@@ -64,7 +71,7 @@ class Buffer:
             self._data[self._next_idx] = transition
         self._next_idx = (self._next_idx + 1) % self._buffer_size
 
-    def sample(self, batch_size: int):
+    def sample(self, batch_size: int) -> Transition:
         idxes = torch.randint(len(self._data) - 1, (batch_size, ))
 
         batch = [self._data[idx] for idx in idxes]
