0dfc2b9a54435f149822426cdb69fa23b8fe6649
d3sm0 <simone.totaro@gmail.com>
2022-01-17 19:56:15 -0500

    Add Actor-critic type

--- rlego/__init__.py
+++ rlego/__init__.py
@@ -1,5 +1,5 @@
 from rlego.src.buffer import Buffer, Trajectory, Transition, check_shape_transition
 from rlego.src.multistep import lambda_returns, discounted_returns
-from rlego.src.policy_gradients import policy_gradient
+from rlego.src.policy_gradients import policy_gradient, ActorCriticType
 from rlego.src.utils import polyak_update
 from rlego.src.value_learning import td_learning, q_learning
