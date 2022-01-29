12d2b3d2217ee166121c4368e29141ad5d9b36c1
d3sm0 <simone.totaro@gmail.com>
2022-01-29 10:42:38 -0500

    Add mdpo

--- rlego/src/policy_gradients.py
+++ rlego/src/policy_gradients.py
@@ -10,9 +10,14 @@ def vanilla_policy_gradient(log_prob: T, q_t: T) -> T:
     return log_prob * q_t.detach()
 
 
-def policy_gradient(dist: torch_dist.Distribution, a_t: T, q_t: T) -> T:
-    action_log_prob = dist.log_prob(a_t.detach()).sum(-1)
+def policy_gradient(pi_t: torch_dist.Distribution, a_t: T, q_t: T) -> T:
+    action_log_prob = pi_t.log_prob(a_t.detach())
     return vanilla_policy_gradient(action_log_prob, q_t)
+
+
+def mdpo(pi_tm1: torch_dist.Distribution, pi_t: torch_dist.Distribution, a_t: T, adv: T) -> T:
+    rho_t = (pi_t.log_prob(a_t) - pi_tm1.log_prob(a_t)).exp()
+    return -(rho_t * adv)
 
 
 class ActorCriticType(torch.nn.Module, abc.ABC):
